26c27470c6da8ac82cb8956a30335fad82e5edb2
higuchiryunosuke <higuchi@mavericks09.com>
2020-04-13 18:01:54 +0900

    スタイル調整

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header><span>Sventouz Tech Blog</span></header>
+</template>
+
+<style>
+  header {
+    background-color: #2c3e50;
+    box-shadow: 0 2px 4px rgba(0,0,0,.2);
+    color: #fff;
+    /* font-family: Patua One,cursive; */
+    padding: 10px 20px;
+    position: relative;
+    text-align: center;
+    font-size: 30px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,23 @@
 <template>
-  <section class="latest-posts">
-    <div class="posts">
-      <nuxt-link :to="'posts/'+post.fields.slug" class="post" v-for="(post, index) in posts" :key="index">
-        <div class="thumb">
-          <img :src="post.fields.image.fields.file.url">
-        </div>
-        <div class="post-text">
-          <p>{{ formatDate(post.sys.createdAt) }}</p>
-          <h2>{{ post.fields.title }}</h2>
-        </div>
-      </nuxt-link>
-    </div>
-  </section>
+  <div class="wrapper">
+    <section class="latest-posts">
+      <div class="posts">
+        <nuxt-link :to="'posts/'+post.fields.slug" class="post" v-for="(post, index) in posts" :key="index">
+          <div class="thumb">
+            <img :src="post.fields.image.fields.file.url">
+          </div>
+          <div class="post-text">
+            <p>{{ formatDate(post.sys.createdAt) }}</p>
+            <h2>{{ post.fields.title }}</h2>
+          </div>
+        </nuxt-link>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import client from '~/plugins/contentful'
+import client from '~/plugins/contentful';
 export default {
   asyncData({ params }) {
     return client
@@ -44,42 +46,32 @@ export default {
 </script>
 
 <style lang="scss">
+.wrapper {
+  padding: 30px 0;
+  background: #eee;
+}
 section.latest-posts {
-  padding: 10px;
   .posts {
     max-width: 900px;
     margin: 0 auto;
-    padding: 10px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    background: #ddd;
     a.post {
-      width: calc(100% / 2 - 20px);
-      @media (min-width: (768px)) {
-        width: calc(100% / 3 - 20px);
-      }
-      margin: 10px;
-      background: #fff;
-      text-decoration: none;
+      display: flex;
+      margin: 0 0 30px;
+      padding: 10px;
       color: #111;
+      text-decoration: none;
+      background: #fff;
+      box-shadow: 2px 2px 5px rgba(0,0,0,.2);
       .thumb {
-        width: 100%;
-        padding-bottom: 75%;
-        position: relative;
-        overflow: hidden;
+        max-width: 180px;
         img {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
           max-width: 100%;
+          border: 1px solid #eee;
         }
       }
       .post-text {
-        padding: 5px 10px 10px;
+        padding: 5px 20px 10px;
         h2 {
-          width: fit-content;
           font-size: 20px;
         }
       }
